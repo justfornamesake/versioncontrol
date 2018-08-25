2d44c26330eba33edf56dcf1647dd0a32f72c33e
satya_jha <satyajeetjha06@gmail.com>
2018-08-25 19:33:46 +0530

    pushing master.js to Master

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Display from './Display';
-
+import Master from './Master';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
           <Display/>
+          <Master/>
       </div>
     );
   }
---
+++ src/Master.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+ export default class Master extends React.Component{
+    render(){
+        return(
+
+            <div>
+
+            <h1> Helllo  this is master </h1>
+             </div>
+        );
+        
+    }
+}
