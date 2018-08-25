bbcffdde9052751b3b026569bfabce4dbda44ec2
satya_jha <satyajeetjha06@gmail.com>
2018-08-25 18:55:50 +0530

    changed in newbranch

---
+++ src/Display.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+ export default class Display extends React.Component{
+    render (){
+
+        return(
+    <div>
+         <h1>This is display componnent </h1>
+        </div>
+
+        );
+    }
+}
